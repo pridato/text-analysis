414dd18d2ffa0c9c53b530790fcfb5fc440d1d73
pridato <davidarroyo25052004@gmail.com>
2025-04-10 18:40:55 +0200

    añadidas bibliotecas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/analyze', methods=['POST'])
+def analyze_text():
+    data = request.get_json()
+    if 'text' not in data:
+        return jsonify({'error': 'No text provided'}), 400
+
+    text = data['text']
 
 if __name__ == '__main__':
     app.run()
